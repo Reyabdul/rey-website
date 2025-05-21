0837cd2a6583d8c2a63022129c302d1b8c3e66ec
Reyabdul <reyabdul@gmail.com>
2025-05-21 15:06:44 +0900

    add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './global.css'
 import App from './App.tsx'
+//React Router
+import { BrowserRouter } from "react-router"
+//Tanstack Query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+//Styles
+import './global.css'
+
+const queryClient = new QueryClient() 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
